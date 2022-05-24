1ad4ad635feb8ec44be767ad5168df01e934417c
urvashiramdasani <urvashi.ramdasani@gmail.com>
2022-05-25 00:09:57 +0530

    React Redux Form Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-
 import { connect } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { addComment, fetchDishes } from '../redux/ActionsCreators';
+import { actions } from 'react-redux-form';
 
 
 const mapStateToProps = state => {
@@ -23,7 +24,8 @@ const mapStateToProps = state => {
 
 const mapDispatchtoProps = (dispatch) => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-    fetchDishes: () => { dispatch(fetchDishes()) }
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 })
 
 function withRouter(Component) {
@@ -54,11 +56,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return(
-                <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} 
-                dishesLoading={this.props.dishes.isLoading}
-                dishesErrMess={this.props.dishes.errMess}
-                promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
-                leader={this.props.leaders.filter((leader) => leader.featured)[0]} />
+                <Home dish={ this.props.dishes.dishes.filter((dish) => dish.featured)[0] } 
+                dishesLoading={ this.props.dishes.isLoading }
+                dishesErrMess={ this.props.dishes.errMess }
+                promotion={ this.props.promotions.filter((promotion) => promotion.featured)[0] }
+                leader={ this.props.leaders.filter((leader) => leader.featured)[0] } />
             );
         }
 
@@ -66,10 +68,10 @@ class Main extends Component {
             let {dishId} = useParams();
             
             return(
-                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(dishId, 10))[0]}
-                isLoading={this.props.dishes.isLoading}
-                errMess={this.props.dishes.errMess}
-                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(dishId, 10))} addComment={this.props.addComment} />
+                <DishDetail dish={ this.props.dishes.dishes.filter((dish) => dish.id === parseInt(dishId, 10))[0] }
+                isLoading={ this.props.dishes.isLoading }
+                errMess={ this.props.dishes.errMess }
+                comments={ this.props.comments.filter((comment) => comment.dishId === parseInt(dishId, 10)) } addComment={ this.props.addComment } />
             );
         }
 
@@ -77,12 +79,12 @@ class Main extends Component {
             <div>
                 <Header />
                 <Routes>
-                    <Route path='/home' element={<HomePage />} />
-                    <Route path="/aboutus" element={<About leaders={this.props.leaders} />} />
-                    <Route exact path="/menu" element={<Menu dishes={this.props.dishes} />} />
-                    <Route path="/menu/:dishId" element={<DishWithId />} />
-                    <Route exact path="/contactus" element={<Contact />} />
-                    <Route path="*" element={<Navigate to="/home" replace />} />
+                    <Route path='/home' element={ <HomePage /> } />
+                    <Route path="/aboutus" element={ <About leaders={ this.props.leaders } /> } />
+                    <Route exact path="/menu" element={ <Menu dishes={ this.props.dishes } /> } />
+                    <Route path="/menu/:dishId" element={ <DishWithId /> } />
+                    <Route exact path="/contactus" element={ <Contact resetFeedbackForm={ this.props.resetFeedbackForm } /> } />
+                    <Route path="*" element={ <Navigate to="/home" replace /> } />
                 </Routes>
                 <Footer />
             </div>
